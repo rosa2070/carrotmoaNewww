a2a29f3f0447f7e28b2e6d6928d2a7e2c054ce43
ddshpp <dshp753@gmail.com>
2024-09-25 14:24:03 +0900

    test: Welcome Page 생성 및 테스트

--- src/main/java/carrotmoa/carrotmoa/CarrotMoaApplication.java
+++ src/main/java/carrotmoa/carrotmoa/CarrotMoaApplication.java
@@ -2,8 +2,11 @@ package carrotmoa.carrotmoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//TODO : 1. DB 연결후 기존 어노테이션에서 주석된 어노테이션으로 변경하기
+//@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CarrotMoaApplication {
 
     public static void main(String[] args) {
