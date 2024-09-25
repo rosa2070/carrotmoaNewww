f719af4d3c7a5f973065685bb160b5181bd828d9
ddshpp <dshp753@gmail.com>
2024-09-25 14:03:12 +0900

    first commit

---
+++ src/main/java/carrotmoa/carrotmoa/CarrotMoaApplication.java
@@ -0,0 +1,13 @@
+package carrotmoa.carrotmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotMoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/carrotmoa/carrotmoa/CarrotMoaApplicationTests.java
@@ -0,0 +1,13 @@
+package carrotmoa.carrotmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotMoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
