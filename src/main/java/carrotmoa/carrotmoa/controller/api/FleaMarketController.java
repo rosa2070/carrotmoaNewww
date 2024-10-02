279afece5053c949dcfe1d0c9986fe65bb08cec7
ddshpp <dshp753@gmail.com>
2024-10-02 17:17:36 +0900

    test: test api 생성

---
+++ src/main/java/carrotmoa/carrotmoa/controller/api/FleaMarketController.java
@@ -0,0 +1,16 @@
+package carrotmoa.carrotmoa.controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/fleamarket")
+@RestController
+public class FleaMarketController {
+
+    @GetMapping
+    public void test() {
+        System.out.println("test");
+    }
+}
