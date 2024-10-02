6636a9ac6ddbb5828eca952c714aac6b39bbf25b
ddshpp <dshp753@gmail.com>
2024-10-02 17:22:15 +0900

    update: reformat code

--- src/main/java/carrotmoa/carrotmoa/CarrotMoaApplication.java
+++ src/main/java/carrotmoa/carrotmoa/CarrotMoaApplication.java
@@ -4,13 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//TODO : 1. DB 연결후 기존 어노테이션에서 주석된 어노테이션으로 변경하기
-//@SpringBootApplication
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class CarrotMoaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CarrotMoaApplication.class, args);
     }
-
 }
