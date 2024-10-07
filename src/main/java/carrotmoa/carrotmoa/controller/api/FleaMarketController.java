91eb3c74819153b0b9d77fb13a588cc6c764108a
ddshpp <dshp753@gmail.com>
2024-10-07 16:03:15 +0900

    feat: 중고거래 게시글 등록 기능

--- src/main/java/carrotmoa/carrotmoa/controller/api/FleaMarketController.java
+++ src/main/java/carrotmoa/carrotmoa/controller/api/FleaMarketController.java
@@ -2,12 +2,14 @@ package carrotmoa.carrotmoa.controller.api;
 
 import carrotmoa.carrotmoa.model.request.SaveFleaMarketPostRequest;
 import carrotmoa.carrotmoa.service.FleaMarketService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "중고거래", description = "중고거래 페이지")
 @RequestMapping("/api/fleamarket")
 @RestController
 public class FleaMarketController {
@@ -17,6 +19,7 @@ public class FleaMarketController {
 
     @GetMapping
     public Long createPost(@RequestBody SaveFleaMarketPostRequest saveFleaMarketPostRequest) {
+        //TODO : 로그인된 유저의 id를 가져오는 과정 추가 필요
        return fleaMarketService.savePost(saveFleaMarketPostRequest);
     }
 }
--- src/main/java/carrotmoa/carrotmoa/model/request/SaveFleaMarketPostRequest.java
+++ src/main/java/carrotmoa/carrotmoa/model/request/SaveFleaMarketPostRequest.java
@@ -1,8 +1,8 @@
 package carrotmoa.carrotmoa.model.request;
 
-import carrotmoa.carrotmoa.entity.Hit;
 import carrotmoa.carrotmoa.entity.Post;
-import jakarta.persistence.Column;
+import carrotmoa.carrotmoa.entity.Product;
+import carrotmoa.carrotmoa.enums.ProductStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,12 +16,29 @@ public class SaveFleaMarketPostRequest {
     private String content;
     private boolean isDeleted;
 
+    private Long postId;
+    private Integer price;
+    private boolean isPriceOffer;
+    private boolean isFree;
+    private ProductStatus status;
+
     public Post toPostEntity() {
         return Post.builder()
             .categoryId(categoryId)
             .userId(userId)
             .title(title)
             .content(content)
+            .isDeleted(false)
+            .build();
+    }
+
+    public Product toProductEntity(Long id) {
+        return Product.builder()
+            .postId(id)
+            .price(price)
+            .isPriceOffer(isPriceOffer)
+            .isFree(isFree)
+            .status(ProductStatus.ON_SALE)
             .build();
     }
 }
