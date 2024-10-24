594143cbde3e03d3d2504b062ab451510de211d9
Yijae <lullulalla9711@gmail.com>
2024-10-24 16:05:52 +0900

    add: 캘린더 드래그로 사용자로부터 숙박일 수 가져오기

--- src/main/resources/static/js/room_booking.js
+++ src/main/resources/static/js/room_booking.js
@@ -6,7 +6,57 @@ document.addEventListener('DOMContentLoaded', function() {
         titleFormat: function (date) {
             return `${date.date.year}년 ${date.date.month + 1}월`;
         },
+        selectable: true,
+        dragScroll: true,
+        validRange: {
+            start: new Date().toISOString().split('T')[0]
+        },
+        select: function(info) {
+            // 선택한 날짜 범위 정보
+            var startDate = info.startStr;
+            var endDate = new Date(info.endStr);
+            endDate.setDate(endDate.getDate() - 1);
+            var selectedEndDate = endDate.toISOString().split('T')[0];
 
+            document.getElementById('checkin-dates').textContent =
+                `${startDate}`;
+            document.getElementById('checkout-dates').textContent =
+                `${selectedEndDate}`;
+
+            var timeDiffernece = selectedEndDate - startDate;
+            var countNights = timeDiffernece / (1000 * 60 * 60 * 24) // 밀리초 단위에서 일로 변환해야함
+
+            document.getElementById('nights-count').textContent = `총 ${countNights}박`;
+
+            var pricePerWeek = document.getElementById('price').getAttribute('data-price');
+            pricePerWeek = parseInt(pricePerWeek, 10);
+
+            var totalPrice = (pricePerWeek / 7) * nights;
+            document.getElementById('total-price').textContent = `${totalPrice.toLocaleString()}원`;
+
+
+        }
     });
     calendar.render();
 });
+
+// // controller로 선택한 날짜 넘기기
+// function sendSelectedDates(startDate, selectedEndDate) {
+//     fetch('/your-controller-endpoint', {
+//         method: 'Post',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             startDate: startDate,
+//             endDate: selectedEndDate
+//         })
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log('Success:', data);
+//         })
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         });
+// }
