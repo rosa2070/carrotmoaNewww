f5425b7c78c9c6519181dcef09bb9c5885356738
Yijae <lullulalla9711@gmail.com>
2024-10-26 20:11:19 +0900

    add: detail 페이지 이용후기 postman으로 데이터 들어오는 건 확인

--- src/main/java/carrotmoa/carrotmoa/controller/api/GuestRoomDetailController.java
+++ src/main/java/carrotmoa/carrotmoa/controller/api/GuestRoomDetailController.java
@@ -1,9 +1,6 @@
 package carrotmoa.carrotmoa.controller.api;
 
-import carrotmoa.carrotmoa.model.response.AccommodationDetailResponse;
-import carrotmoa.carrotmoa.model.response.AmenityImageResponse;
-import carrotmoa.carrotmoa.model.response.SpaceImageResponse;
-import carrotmoa.carrotmoa.model.response.UserProfileResponse;
+import carrotmoa.carrotmoa.model.response.*;
 import carrotmoa.carrotmoa.service.GuestRoomDetailService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 @Controller
@@ -26,10 +24,12 @@ public class GuestRoomDetailController {
         List<AmenityImageResponse> amenities = guestRoomDetailService.getAmenityImage(id);
         List<SpaceImageResponse> icons = guestRoomDetailService.getSpaceImage();
         List<UserProfileResponse> profile = guestRoomDetailService.getHostProfile(id);
+        List<AccommodationReviewResponse> reviews = guestRoomDetailService.getAllReview(id);
         model.addAttribute("room", getRoomDetailById);
         model.addAttribute("amenities", amenities);
         model.addAttribute("icons", icons);
         model.addAttribute("profile", profile);
+        model.addAttribute("reviews", reviews);
         // 링크 복사를 위해서 model에 담아주기
         String copyLinkUrl = "http://localhost:8080/room/detail/" + id;
         model.addAttribute("copyLinkUrl", copyLinkUrl);
@@ -37,11 +37,8 @@ public class GuestRoomDetailController {
     }
 
 //    @GetMapping("/{id}") // postman test
-//    public List<UserProfileResponse> getAmenities(@PathVariable("id") long id) {
-//        AccommodationDetailResponse getRoomDetailById = guestRoomDetailService.getAccommodationDetail(id);
-//        List<AmenityImageResponse> amenities = guestRoomDetailService.getAmenityImage(id);
-//        List<SpaceImageResponse> icons = guestRoomDetailService.getSpaceImage();
-//        List<UserProfileResponse> profile = guestRoomDetailService.getHostProfile(id);
-//        return profile;
+//    public List<AccommodationReviewResponse> getAmenities(@PathVariable("id") long id) {
+//        List<AccommodationReviewResponse> reviews = guestRoomDetailService.getAllReview(id);
+//        return reviews;
 //    }
 }
---
+++ src/main/java/carrotmoa/carrotmoa/model/response/AccommodationReviewResponse.java
@@ -0,0 +1,33 @@
+package carrotmoa.carrotmoa.model.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@Slf4j
+public class AccommodationReviewResponse {
+    private long id;
+    private long userId;
+    private int rating;
+    private String comment;
+    private LocalDate updatedAt;
+    private String nickname;
+
+    public static AccommodationReviewResponse fromData(Object[] data){
+        return AccommodationReviewResponse.builder()
+                .id((long) data[0])
+                .userId((long) data[1])
+                .rating((int) data[2])
+                .comment((String) data[3])
+                .updatedAt(((LocalDateTime) data[4]).toLocalDate())
+                .nickname((String) data[5])
+                .build();
+    }
+}
