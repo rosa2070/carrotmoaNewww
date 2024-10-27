0e5b99b342738cc5895e42c5931f03d833701dd2
jaeeun <rosa2070@naver.com>
2024-10-27 19:15:50 +0900

    add:결제 기능 테스트 시작

---
+++ src/main/java/carrotmoa/carrotmoa/controller/view/PaymentViewController.java
@@ -0,0 +1,15 @@
+package carrotmoa.carrotmoa.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/payment")
+public class PaymentViewController {
+
+    @GetMapping
+    public String Payment() {
+        return "payment";
+    }
+}
