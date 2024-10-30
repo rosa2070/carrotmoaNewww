f310ca5a6eae37fd19f434852f031a229f6ae883
Yijae <lullulalla9711@gmail.com>
2024-10-30 17:47:58 +0900

    fix: 총 숙박일 해결, 체크인 날짜 조정 해결

--- src/main/resources/static/js/room_booking.js
+++ src/main/resources/static/js/room_booking.js
@@ -60,15 +60,28 @@ document.addEventListener('DOMContentLoaded', function() {
                 },
                 events: events, // 예약된 날짜를 events로 추가
                 select: function(info) {
+                    // 하루만 선택하는 거 불가
+                    const endDate = new Date(info.end.getFullYear(), info.end.getMonth(), info.end.getDate());
+                    endDate.setDate(info.end.getDate() - 1);
+                    console.log(endDate);
+
+                    if(info.start.getDate() == endDate.getDate()) {
+                        alert('최소 1박이상 가능합니다, 하루 이상 선택해주세요.');
+                        calendar.unselect();
+                        return;
+                    }
+
                     // var checkInDate = info.startStr; // 문자열로 받아오는 방식
                     var checkInDate = info.start; // Date 객체로 받아오는 방식
+                    var updatedCheckInDate = new Date(checkInDate);
                     var checkOutDate = info.end;
+                    updatedCheckInDate.setDate(updatedCheckInDate.getDate() + 1);
 
-                    document.getElementById('checkin-dates').textContent = checkInDate.toISOString().split('T')[0];
+                    document.getElementById('checkin-dates').textContent = updatedCheckInDate.toISOString().split('T')[0];
                     document.getElementById('checkout-dates').textContent = checkOutDate.toISOString().split('T')[0];
 
                     var timeDifference = checkOutDate - checkInDate;
-                    var countNights = ((timeDifference) / (1000 * 60 * 60 * 24)) + 1;
+                    var countNights = ((timeDifference) / (1000 * 60 * 60 * 24));
                     document.getElementById('nights-count').textContent = countNights;
                 },
                 unselect: function() {
