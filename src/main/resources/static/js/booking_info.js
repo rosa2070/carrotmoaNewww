03da8357a05bee20dacaf548fc7f65baf6bc029a
Yijae <lullulalla9711@gmail.com>
2024-10-31 12:10:24 +0900

    fix: bookingStart 페이지에 데이터 전부 들어옴

--- src/main/resources/static/js/booking_info.js
+++ src/main/resources/static/js/booking_info.js
@@ -1,10 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('click', function (event) {
     const title = document.getElementById('room-info-title').textContent;
     const addr = document.getElementById('room-addr-info').textContent;
     const name = document.getElementById('host_name').textContent;
     const price = parseFloat(document.getElementById('room_info_price').textContent);
-    const night = parseFloat(document.getElementById('nights-count').textContent, 10);
+    // const night = document.getElementById('nights-count');
+    const nightsCountElement = document.getElementById('nights-count');
+    const nightsCountText = nightsCountElement.textContent.trim(); // 공백 제거
+    const night = parseFloat(nightsCountText);
     const totalPrice = price * night;
+    console.log(price);
+    console.log(night);
     console.log(totalPrice);
 
     sessionStorage.setItem('room-info-title', title);
