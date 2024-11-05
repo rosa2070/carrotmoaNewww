73253fece0d92f336eb721205977214278b4cb90
Yijae <lullulalla9711@gmail.com>
2024-11-05 17:07:55 +0900

    add: 지도 test cluster, marker 들어옴

---
+++ src/main/java/carrotmoa/carrotmoa/controller/view/SearchMapController.java
@@ -0,0 +1,12 @@
+package carrotmoa.carrotmoa.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchMapController {
+    @GetMapping("/search/map")
+    public String gotoSearchMap() {
+        return "kakaomap/kakaomap-accommodation";
+    }
+}
