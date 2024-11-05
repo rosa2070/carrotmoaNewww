7bec7c6d6029dd0a4a8fb7064dc79133014e2c02
jaeeun <rosa2070@naver.com>
2024-11-05 10:54:22 +0900

    fix: 빈 계약 목록 불러올 시 404 오류 안나도록 수정

--- src/main/java/carrotmoa/carrotmoa/controller/api/HostReservationController.java
+++ src/main/java/carrotmoa/carrotmoa/controller/api/HostReservationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -27,7 +28,7 @@ public class HostReservationController {
         List<HostReservationResponse> hostReservations = hostReservationService.getHostReservations(userId);
 
         if (hostReservations.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            return ResponseEntity.ok(Collections.emptyList()); // 200 OK와 빈 배열 반환
         }
 
         return ResponseEntity.ok(hostReservations);
